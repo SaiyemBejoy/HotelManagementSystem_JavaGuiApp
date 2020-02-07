af29c188fa4afeeec9c5b8c8976a287f49694bfd
SaiyemBejoy <jsbejoy96@gmail.com>
2020-02-07 20:11:26 +0600

    Initial commit

---
+++ src/hotel/Hotel.java
@@ -0,0 +1,43 @@
+package hotel;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Hotel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Hotel window = new Hotel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Hotel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
